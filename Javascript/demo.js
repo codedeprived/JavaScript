8ab6a732251fb1eb990a30a39da2d84103b7d274
codedeprived <smanmohan735@gmail.com>
2024-09-18 20:29:22 +0530

    day1-Project

---
+++ Javascript/demo.js
@@ -0,0 +1,4 @@
+console.log("Prices:\nBubblegum: $2\nToffee: $0.2\nIce cream: $5\nMilk chocolate: $4\nDoughnut: $2.5\nPancake: $3.2")
+let income = 2 + 0.2 + 5 + 4 + 2.5 +  3.2;
+console.log(`\n\nIncome:  $${income}`);
+let $disabled;
